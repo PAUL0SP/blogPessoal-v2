6712189a35ab09687fc43dea38094dd032faba68
unknown <paulo.dm9@gmail.com>
2022-03-03 16:26:12 -0300

    implimentação doget e post de temas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 
@@ -22,6 +23,7 @@ import { InicioComponent } from './inicio/inicio.component';
     EntrarComponent,
     CadastrarComponent,
     InicioComponent,
+    TemaComponent,
     
   ],
   imports: [
