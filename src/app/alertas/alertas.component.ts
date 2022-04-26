29f6b26b1a833463a39b0086c32ed9fb1ed6703f
unknown <paulo.dm9@gmail.com>
2022-04-26 11:34:29 -0300

    Implementando alertas reutiizaveis

---
+++ src/app/alertas/alertas.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { __importDefault } from 'tslib';
+
+@Component({
+  selector: 'app-alertas',
+  templateUrl: './alertas.component.html',
+  styleUrls: ['./alertas.component.css']
+})
+export class AlertasComponent implements OnInit {
+
+@Input() message: string
+@Input() tipo = 'success'
+
+  constructor(
+    public modal: BsModalRef
+  ) { }
+
+  ngOnInit(){
+  }
+
+  onClose(){
+    this.modal.hide()
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,9 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
+import { AlertasComponent } from './alertas/alertas.component';
+
 
 
 
@@ -32,13 +36,16 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     TemaDeleteComponent,
     PostagemEditComponent,
     PostagemDeleteComponent,
+    UserEditComponent,
+    AlertasComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ModalModule.forRoot()
   ],
   providers: [{
     provide: LocationStrategy,
